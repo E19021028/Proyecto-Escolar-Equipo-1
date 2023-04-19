81526bd93db26314e693200ca383806a303eea88
Nesme <L18020778@veracruz.tecnm.mx>
2023-04-19 00:30:56 -0600

    Actualizar Especialidad

--- src/Especialidad.java
+++ src/Especialidad.java
@@ -1,3 +1,20 @@
 public class Especialidad {
-
+    private int id;
+    private String nombre;
+    public Especialidad(int id,String nombre){
+        this.id=new id;
+        this.nombre=new nombre;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
