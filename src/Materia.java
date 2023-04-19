6f1f405edd9eecde42e92580f7b521d1d36881af
DanielAlbertoAlmendraGuzman <l19021028@veracruz.tecnm.mx>
2023-04-19 00:30:53 -0600

    Getters y setters colocados

--- src/Materia.java
+++ src/Materia.java
@@ -1,4 +1,24 @@
 public class Materia {
     private int id;
     private String nombre;
+    public int getId(){
+        return  id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Materia(int id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
 }
