e1103530b583056c156f8cc564dc17d0d4f196bf
Valeria Socken <L19021144@veracruz.tecnm.mx>
2023-04-19 09:02:31 -0600

    Actualizar Catedratico

--- src/catedratico.java
+++ src/catedratico.java
@@ -1,3 +1,31 @@
-public class catedratico{
+import java.io.Serializable;
+
+public class Catedratico implements Serializable {
+
+    private String RFC;
+
+    private String nombre;
+
+    public  Catedratico(String RFC,String nombre){
+        this.RFC= RFC;
+        this.nombre= nombre;
+
+    }
+
+    public String getRFC() {
+        return RFC;
+    }
+
+    public void setRFC(String RFC) {
+        this.RFC = RFC;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
 }
